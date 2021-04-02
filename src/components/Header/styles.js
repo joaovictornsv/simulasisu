47a75abe3249188bcf0654ca965d0be0b4faa658
vinimedeiros <vinimedeiros7@outlook.com>
2021-04-02 10:44:58 -0300

    Alterando a cor do header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,8 +1,14 @@
 import styled from 'styled-components'
 
 export const HeaderBase = styled.header`
-  background-color: purple;
+  background-color: #2b2e4a;
   height: 60vh;
   width: 100%;
   color: white;
+  
+`
+export const HeaderTitle = styled.h1`
+  margin: 0;
+  padding: 20px;
+  
 `
