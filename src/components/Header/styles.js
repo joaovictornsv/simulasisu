f81d5ba568f459d08694dc079554b71fefd13e89
Joao Victor Negreiros <joaovictornsv@gmail.com>
2021-04-02 16:12:34 -0300

    adicionado imagens e menu no header

--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,14 +1,45 @@
 import styled from 'styled-components'
 
-export const HeaderBase = styled.header`
-  background-color: #2b2e4a;
+const HeaderBase = styled.header`
+  background-color: #0e49b5;
   height: 60vh;
   width: 100%;
   color: white;
-  
+  border-bottom-right-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
 `
-export const HeaderTitle = styled.h1`
+
+HeaderBase.Top = styled.div`
+  height: auto;
+  padding: 10px;
+  width: 90%;
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-start;
+  margin-top: 30px;
+`
+
+HeaderBase.Content = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  margin-bottom: 30px;
+`
+
+HeaderBase.Title = styled.h2`
   margin: 0;
   padding: 20px;
-  
+  font-family: 'Roboto', sans-serif;
 `
+
+HeaderBase.Image = styled.img`
+  width: 70vw;
+  max-width: 400px;
+  height: auto;
+`
+
+export default HeaderBase
---
+++ src/components/MenuMobile/index.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import Menu from '@material-ui/core/Menu'
+import MenuItem from '@material-ui/core/MenuItem'
+import MenuList from '@material-ui/core/MenuList'
+import { makeStyles } from '@material-ui/core/styles'
+import { GrMenu } from 'react-icons/gr'
+
+const useStyles = makeStyles(() => ({
+  item: {
+    padding: 0,
+    textAlign: 'center',
+    display: 'flex',
+    justifyContent: 'center'
+  },
+  list: {
+    width: '80vw',
+    outline: 'none'
+  }
+}))
+
+export default function SimpleMenu () {
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget)
+  }
+
+  const handleClose = () => {
+    setAnchorEl(null)
+  }
+
+  const classes = useStyles()
+
+  return (
+    <div>
+      <button onClick={handleClick}>
+        <GrMenu />
+      </button>
+      <Menu
+        id="simple-menu"
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        <MenuList className={classes.list}>
+          <MenuItem className={classes.item} onClick={handleClose}>Home</MenuItem>
+          <MenuItem className={classes.item} onClick={handleClose}>Simular</MenuItem>
+          <MenuItem className={classes.item} onClick={handleClose}>Calcular</MenuItem>
+          <MenuItem className={classes.item} onClick={handleClose}>Sobre</MenuItem>
+        </MenuList>
+      </Menu>
+    </div>
+  )
+}
